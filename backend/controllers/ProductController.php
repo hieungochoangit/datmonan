8f2284b2a747befb99061a1f797f3ad13e48677c
hieungochoangit <hieungochoangit@gmail.com>
2021-01-31 01:05:25 +0700

    create form add product

--- backend/controllers/ProductController.php
+++ backend/controllers/ProductController.php
@@ -14,7 +14,24 @@ class ProductController extends Controller {
 		$partner = new Partner();
 		$storeList = $partner->getAllStoreActive();
 
-		$this->content = $this->view("views/product/add.php", ['cateList' => $cateList, 'storeList' => $storeList]);
+		// Submit
+		if (isset($_POST['submit'])) {
+			$name = $_POST['name'];
+			$thumbnail = $_FILES['thumbnail'];
+			$price = $_POST['price'];
+			$sale = $_POST['sale'];
+			$desc = $_POST['desc'];
+			$status = $_POST['status'];
+			$category = $_POST['category'];
+			$store = $_POST['store'];
+
+			// Validate
+			if (empty($name) || $thumbnail['error'] != 0 || empty($price) || empty($desc)) {
+				$this->error = "Vui lòng nhập đầy đủ";
+			}
+		}
+
+		$this->content = $this->view("views/product/add.php", ['cateList' => $cateList, 'storeList' => $storeList, 'error' => $this->error]);
 		include "views/layouts/content.php";
 	}
 }
--- backend/views/product/add.php
+++ backend/views/product/add.php
@@ -1,31 +1,40 @@
 <h1>Thêm sản phẩm</h1>
 
-<div class="row">
-	<div class="col-md-5">
-		<form action="">
+<form action="" method="POST" enctype="multipart/form-data">
+	<div class="row">
+		<div class="col-md-6">
 			<div class="form-group">
 				<label for="">Tên sản phẩm</label>
-				<input type="text" class="form-control">
+				<input name="name" type="text" class="form-control">
 			</div>
 			<div class="form-group">
 				<label for="">Ảnh sản phẩm</label>
-				<input type="file" class="form-control">
+				<input name="thumbnail" type="file" class="form-control">
 			</div>
 			<div class="form-group">
 				<label for="">Giá sản phẩm</label>
-				<input type="text" class="form-control">
+				<input name="price" type="text" class="form-control">
 			</div>
 			<div class="form-group">
 				<label for="">Giá đã giảm</label>
-				<input type="text" class="form-control">
+				<input name="sale" type="text" class="form-control">
 			</div>
 			<div class="form-group">
 				<label for="">Mô tả sản phẩm</label>
-				<textarea name="" class="form-control"></textarea>
+				<textarea name="desc" class="form-control"></textarea>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="form-group">
+				<label for="">Trạng thái</label>
+				<select name="status" id="" class="form-control">
+					<option value="0">Hiện</option>
+					<option value="1">Ẩn</option>
+				</select>
 			</div>
 			<div class="form-group">
 				<label for="">Danh mục</label>
-				<select name="" id="" class="form-control">
+				<select name="category" id="" class="form-control">
 					<?php foreach($cateList as $category) : ?>
 						<option value="<?php echo $category['id'] ?>"><?php echo $category['category_name'] ?></option>
 					<?php endforeach ?>
@@ -33,15 +42,16 @@
 			</div>
 			<div class="form-group">
 				<label for="">Cửa hàng</label>
-				<select name="" id="" class="form-control">
+				<select name="store" id="" class="form-control">
 					<?php foreach($storeList as $store) : ?>
 						<option value="<?php echo $store['id'] ?>"><?php echo $store['partner_name'] ?></option>
 					<?php endforeach ?>
 				</select>
 			</div>
 			<div class="form-group">
-				<button class="btn btn-success">Thêm</button>
+				<button name="submit" type="submit" class="btn btn-success">Thêm</button>
 			</div>
-		</form>
+		</div>
 	</div>
-</div>
+</form>
+<h2 style="color: red"><?php echo isset($error) ? $error : '' ?></h2>
