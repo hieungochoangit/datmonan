44a904398d303807d3b8e7d82ca3d3b3b47328ad
hieungochoangit <hieungochoangit@gmail.com>
2021-02-02 22:02:51 +0700

    display detail user

--- frontend/views/user/detail.php
+++ frontend/views/user/detail.php
@@ -5,31 +5,25 @@
                 <article>
                     <div class="chef-item">
                         <div class="chef-inner">
-                            <div class="chef-thumb">
-                                <img src="assets/images/chef/01.jpg" alt="food-chef">
-                            </div>
-                            <div class="chef-content">
-                                <div class="chef-author">
+                            <div style="padding: 222px 20px 0;" class="chef-content">
+                                <div style="top: 10px;" class="chef-author">
                                     <img src="assets/images/chef/author/09.jpg" alt="chef-author">
                                 </div>
                                 <div class="chef-desc">
                                     <div class="chef-desc-top">
-                                        <div class="title">
-                                            <h5>Sanzida laila Trisha</h5>
-                                            <p>Month Top <a href="#">Homechef</a></p>
+                                        <div class="title mb-2">
+                                            <h5>
+                                                <?php echo $_SESSION['user'] ? $_SESSION['user']['user_email'] : ''; ?>
+                                            </h5>
                                         </div>
                                         <div class="scocial-share">
                                             <a href="#" class="food-btn"><span><i class="icofont-ui-user"></i> follow</span></a>
                                         </div>
                                     </div>
                                     <div class="chef-desc-middle">
-                                        <p>I love to cook for my friends and family . Special expertise of Making Pitha , achar , local food and have passonate to produce and promote organic food...</p>
-                                        <ul>
-                                            <li><span>Location</span>: 240 Block Road Mohakhali 1209</li>
-                                            <li><span>Joined</span>: 02 December 2018</li>
-                                            <li><span>Delivered</span>: 248+ Orders</li>
-                                            <li><span>Earned</span>: Tk 1,21,000/=</li>
-                                        </ul>
+                                        <a href="">Thay đổi thông tin</a>
+                                        <span>|</span>
+                                        <a href="">Kiểm tra đơn hàng của bạn</a>
                                     </div>
                                 </div>
                             </div>
